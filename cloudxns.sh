4cb76318feb8cd0829ab635774d8ec73d1aa261a
James Swineson <jamesswineson@gmail.com>
2016-08-15 10:11:05 +0800

    Updated config file fomat

--- cloudxns.sh
+++ cloudxns.sh
@@ -1,32 +1,38 @@
 #!/bin/sh
 
-CONFIG=$1
+CONFIG=${1:-${HOME}/.config/cloudxns/cloudxns.conf}
 
 if [ ! -f "$CONFIG" ];then
-    echo "ERROR, CONFIG NOT EXIST."
+    echo "Config file not found." >&2
     exit 1
 fi 
 
-agent="WeiCN DDNS Client"
-
+agent="CloudXNS DDNS Bash Client"
 URL_D="https://www.cloudxns.net/api2/ddns"
-DATE=$(date)
 
 DdnsCheck() {
-	PARAM_BODY="{\"domain\":\"${1}\"}"
-	if [ x."${2}" != "x." ]; then
-		PARAM_BODY="{\"domain\":\"${2}.${1}\"}"
+	if [ $# -eq 0 ]; then
+		echo 'Usage: $0 "root.domain" ["hostname"]'
+		exit 1
 	fi
-	echo $PARAM_BODY
+
+	FULL_DOMAIN="${1}"
+	if [ x."${2}" != "x." ]; then
+		FULL_DOMAIN="${2}.${1}"
+	fi
+	PARAM_BODY="{\"domain\":\"${FULL_DOMAIN}\"}"
+
+	echo "Updating domain ${FULL_DOMAIN} ..." >&2
 	HMAC_U=$(echo -n "$API_KEY$URL_D$PARAM_BODY$DATE$SECRET_KEY"|md5sum|cut -d" " -f1)
 	RESULT=$(curl --noproxy "*" -A $agent -k -s $URL_D --data $PARAM_BODY -H "API-KEY: $API_KEY" -H "API-REQUEST-DATE: $DATE" -H "API-HMAC: $HMAC_U" -H 'Content-Type: application/json')
-	echo $RESULT
-
-	if [ $(echo -n "$RESULT"|grep -o "message\":\"success\""|wc -l) = 1 ];then
-	    echo "$(date) -- Update success"
+	
+	if echo -n "$RESULT" | grep -o "success" >/dev/null 2>&1; then
+		echo "Update success for domain ${FULL_DOMAIN}" >&2
+		return 0
 	else
-	    echo "$(date) -- Update failed"
+		echo "Update failed for domain ${FULL_DOMAIN}: $RESULT" >&2
+		return 1
 	fi
 }
 
-. $CONFIG
+source $CONFIG
